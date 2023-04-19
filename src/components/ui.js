7fb31e1731fd7bab9c24b1a445badafeeb98552a
Reddyveerendra <reddyveerendra535@gmail.com>
2023-04-19 09:05:17 +0530

    ui

--- src/components/ui.js
+++ src/components/ui.js
@@ -1,27 +1,8 @@
+/* eslint-disable no-unused-vars */
 import React from "react"
-import { Info } from "./data"
 export const Ui = () => {
     const [currentScore, setCurrentScore] = React.useState(0)
-    const [highScore, sethighScore] = React.useState(0)
-    const [cards, setCards] = React.useState([])
-    const [level, setLevel] = React.useState(1)
-    const [data, setData] = React.useState([...Info])
-    function shuffle(data) {
-        for (let i = 0; i < data.length; i++) {
-            const temp = data[i]
-            const j = Math.floor(Math.random() * (data.length - 0) + 0)
-            data[i] = data[j]
-            data[j] = temp;
-        }
-        return data
-    }
-    function Card(name) {
-        return (
-            <div className="card">
-                <img src='../images/{}' alt="name" />
-            </div>
-        )
-    }
+    const [highScore, setHighScore] = React.useState(0)
     return (
         <div>
             <header>
@@ -34,8 +15,9 @@ export const Ui = () => {
                 </div>
             </header>
             <main>
-                <div>{shuffle(data.slice(0, 6))}</div>
-                <div>{data}</div>
+                <div>
+
+                </div>
             </main>
             <footer>
                 <p>Created by Reddy Veerendra</p>
